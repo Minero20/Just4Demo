009382b63ebb55f80f771b359a5f7961de63758c
pisit.namsiri@gmail.com <pisit.namsiri@gmail.com>
2023-10-16 11:28:16 +0700

    11.28

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3243;
 
 app.get('/', (req, res) => {
-    res.send('Change to Github action Test Yeah!!!');
+    res.send('Test One Time For Make sure !!!!');
 });
 
 app.listen(port, () => {
